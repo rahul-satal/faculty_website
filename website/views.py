1b28c13466d2c0f3515b9d9d43528d21ceedde25
rahul-satal <rahulsatal92@gmail.com>
2015-08-17 19:53:27 +0530

    Header/ Footer created

--- website/views.py
+++ website/views.py
@@ -3,14 +3,7 @@ from django.shortcuts import render
 from .models import *
 
 
-
-def index(request):
-	gen_info1 = GenInfo.objects.all()
-	teacher_list = Myteacher.objects.all()
-	context = {'teacher_list':teacher_list,'gen_info1':gen_info1}
-	return render(request, 'website/index.html', context)
-
 def index(request):
 	profile = Profile.objects.all()
 	context = {'profile':profile}
-	return render(request, 'website/header.html', context)
+	return render(request, 'website/index.html', context)
