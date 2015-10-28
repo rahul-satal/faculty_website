8987bdf8695ea81e498d0dda6428a71cfed8ca58
rahul-satal <rahulsatal92@gmail.com>
2015-10-29 00:24:05 +0530

    signup started

--- Pbas/urls.py
+++ Pbas/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import url
 from django.conf import settings
 from Pbas import views
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns('',
     url(r'^Index$', views.login, name='pbas_index'),
     url(r'^Home$', views.home_page, name='home_page'),
+
+
+    
     '''
     url(r'^Index$', views.pbas_index, name='pbas_index'),
     url(r'^Index$', views.pbas_index, name='pbas_index'),
@@ -24,4 +28,4 @@ urlpatterns = [
     url(r'^Index$', views.pbas_index, name='pbas_index'),'''
  
 
-    ]
+    )
--- Pbas/views.py
+++ Pbas/views.py
@@ -4,31 +4,17 @@ from website.models import *
 
 name = "shaligram.prajapat@gmail.com"
 
-'''def pbas_index(request):
-	user_list = Userinfo.objects.all()
-	context = {'user_list':user_list}
-	return render(request, 'pbas/index.html',context)'''
 
 def login(request):
 	#user_list = Userinfo.objects.all()
 	if request.method == 'GET':
 		return render(request, 'pbas/index.html')
 	if request.method == 'POST':
-		#return HttpResponse([request.POST['user_id'],request.POST['pwd']])
 		current_user_object = Userinfo.objects.all().filter(user_id = request.POST['user_id'])
 		if current_user_object:
 			if current_user_object[0].pwd == request.POST['pwd']:
 				return render(request, 'pbas/yearModal.html')
 			 
-	'''
-	if request.method == 'GET':
-		return render(request, 'pbas/index.html')
-	else:
-		'' -----fetching the data from the user and saving it in database---- ''
-		data = contactus(user_id= request.POST['user_id'], pwd= request.POST['pwd'])
-		data.save()
-		return HttpResponseRedirect('#')	
-'''
 def home_page(request):
 	return render(request, 'pbas/home.html')
 
